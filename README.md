9e3bb855228c94cf4d8b6e9b96b2cb0075a6b931
Divya Sinha <divyasinha31@gmail.com>
2023-06-20 23:00:48 +0530

    Added comments and updated the read file

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ UI Project for Acceldata
 2. Use Web audio API by JS to visualize the audio
 3. Handle errors
 4. Beautify canvas to make the visulaizations aesthetic
+
+---
+
+## Project Details
+1. view.html - This file includes the view seen on the browser with all the elements.
+- The "container" div is responsible for uploading file and displaying errors if any.
+- The "canvas" tag is responsible for displaying the visualization post the audio file is uploaded.
+- The "audio" tag is responsible for controlling the uploaded audio file for various actions.
+
+2. style.css - This file has all the styles added to the UI elements.
+- It includes styles for ids and classes.
+
+3. script.js - This file holds the functionality to the UI elements.
+- The class "CanvasCreator" is responsible for creating the canvas on the browser.
+- The class "ErrorHandler" is responsible for displaying error messages for different errors.
+- An "onchange" event is added to the file upload input.
+- Event listeners are added to the "container" div for adding styles and checking when the file has been dropped.
--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let error = document.getElementById("error");
 let canvas = document.getElementById("visual");
 let audio = document.getElementById("audio");
 
+// Class to create the visual for the audio file
 class CanvasCreator {
 	createCanvas(audioFile) {
-		audio.src = URL.createObjectURL(audioFile);
+		audio.src = URL.createObjectURL(audioFile);	// Setting the source to the audio controller
 
 		const audioContext = new AudioContext();
 		const audioAnalyser = audioContext.createAnalyser();
@@ -18,20 +19,22 @@ class CanvasCreator {
 		const bufferLength = audioAnalyser.frequencyBinCount;
 		const dataArr = new Uint8Array(bufferLength);
 
+		// Removing the file upload container and displaying the canvas
 		container.style.display = "none";
 		canvas.style.display = "block";
 
-		function renderFrame() {
+		// Function to render canvas with colourful bars
+		function renderCanvas() {
 			const canvasContext = canvas.getContext("2d");
 			const canvasWidth = canvas.width;
 			const canvasHeight = canvas.height;
 
-			const barWidth = (canvasWidth / bufferLength) * 13;
+			const barWidth = (canvasWidth / bufferLength) * 10;
 
 			let barHeight;
 			let x = 0;
 
-			requestAnimationFrame(renderFrame);
+			requestAnimationFrame(renderCanvas); // Method to get an updated animation before the next one.
 
 			audioAnalyser.getByteFrequencyData(dataArr);
 
@@ -41,40 +44,52 @@ class CanvasCreator {
 			let r, g, b;
 			let bars = 80 // Number of bars to be displayed per frame
 
+			// Setting colours to the bars
 			for (let i = 0; i < bars; i++) {
 				barHeight = (dataArr[i] * 2.5);
 		
 				if (dataArr[i] > 210){ // Pink
-					r = 250
-					g = 0
-					b = 255
+					r = 250;
+					g = 0;
+					b = 255;
 				} else if (dataArr[i] > 200){ // Yellow
-					r = 250
-					g = 255
-					b = 0
+					r = 250;
+					g = 255;
+					b = 0;
 				} else if (dataArr[i] > 190){ // Green
-					r = 204
-					g = 255
-					b = 0
+					r = 205;
+					g = 255;
+					b = 0;
 				} else if (dataArr[i] > 180){ // Cyan
-					r = 0
-					g = 219
-					b = 131
+					r = 0;
+					g = 219;
+					b = 130;
 				} else { // Blue
-					r = 0
-					g = 199
-					b = 255
+					r = 0;
+					g = 200;
+					b = 255;
 				}
 		
 				canvasContext.fillStyle = `rgb(${r},${g},${b})`;
 				canvasContext.fillRect(x, (500 - barHeight), barWidth, barHeight);
 		
-				x += barWidth + 10
+				x += barWidth + 10;
 			}
 		}
 
 		audio.play();
-		renderFrame();
+		renderCanvas();
+	}
+}
+
+// Class to handle possible errors during uploading of file
+class ErrorHandler {
+	multipleFileError() {
+		error.innerText = "Please upload a single audio file."
+	}
+
+	fileMismatchError() {
+		error.innerText = "Please upload a file of an audio format."
 	}
 }
 
@@ -121,9 +136,19 @@ container.addEventListener(
     e.preventDefault();
     e.stopPropagation();
     container.classList.remove("active");
-		const audioFile = e.dataTransfer.files[0];
-		const creator = new CanvasCreator();
-		creator.createCanvas(audioFile);
+		const files = e.dataTransfer.files;
+
+		if (files.length > 1) {
+			const errorHandler = new ErrorHandler();
+			errorHandler.multipleFileError();
+		} else if (files[0].type.split("/")[0] !== "audio") {
+			const errorHandler = new ErrorHandler();
+			errorHandler.fileMismatchError();
+		} else {
+			const audioFile = files[0];
+			const creator = new CanvasCreator();
+			creator.createCanvas(audioFile);
+		}
   },
   false
 );
@@ -131,7 +156,4 @@ container.addEventListener(
 window.onload = () => {
   error.innerText = "";
 	canvas.style.display = "none";
-	// Handle file type error
-	// Handle multiple file error
 };
-
